0fa403e6e16df7382caf4b56319049761c6b3424
epicestudar <viniciusftza@gmail.com>
2023-09-06 13:43:59 -0300

    criação da pasta das calculadoras

---
+++ Calculadoras/src/Calculadora_Idade.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Calculadora_Idade extends JPanel{
+    public Calculadora_Idade() {
+        super();
+        this.add(new JButton("Calculadora 1"));
+    }
+}
