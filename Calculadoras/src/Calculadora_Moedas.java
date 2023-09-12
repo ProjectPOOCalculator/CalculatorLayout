0845bb470fdc606a8fa67d39458a262feba0394f
epicestudar <viniciusftza@gmail.com>
2023-09-12 16:12:15 -0300

    layout

--- Calculadoras/src/Calculadora_Moedas.java
+++ Calculadoras/src/Calculadora_Moedas.java
@@ -11,19 +11,29 @@ public class Calculadora_Moedas extends JPanel{
         this.add(painel2);
 
         //criando e adicionando os componentes ao painel1
-        painel1.add(new JLabel("CALCULATOR DÓLAR TO REAL"), BorderLayout.NORTH);
-        painel1.add(new JTextArea(2, 10), BorderLayout.NORTH);
-        painel1.add(painel2, BorderLayout.CENTER);
+
+        JLabel label = new JLabel("CALCULATOR DÓLAR TO REAL");
+        painel1.add(label, BorderLayout.NORTH);
+        
+        JTextArea title = new JTextArea(2,10);
+        painel1.add(title, BorderLayout.SOUTH);
+
 
         //criando e adicionando os componentes ao painel2
-        painel2.add(new JLabel("Valor em Dólar:"));
-        painel2.add(new JTextField(10));
-        painel2.add(new JButton("Calcular"));
+        JLabel label2 = new JLabel("Valor em Dólar");
+        painel2.add(label2);
 
-        
-        
+        JTextField texto = new JTextField(10);
+        painel2.add(texto);
 
-        
-        
+        //customização do botao1
+        JButton botao1 = new JButton("Calcular");
+        painel2.add(botao1);
+        botao1.setBackground(Color.GRAY);
+        botao1.setFont(new Font("Arial", Font.BOLD, 16));
+
+        //adicionando o painel 2 ao painel 1
+        painel1.add(painel2, BorderLayout.CENTER);
+
     }
 }
